5b62b113738e2277bb664e6516be15befd10d52b
Jeffrey West <runningwest@gmail.com>
2020-10-20 12:52:02 -0400

    Added GattacaExample

---
+++ GattacaExample/Cell0D.java
@@ -0,0 +1,52 @@
+package GattacaExample;
+
+import HAL.GridsAndAgents.Agent0D;
+
+class Cell0D extends Agent0D<ExampleModel0d> {
+
+    // each cell carries its own Gattaca genome:
+    Gattaca genome;
+
+    Cell0D Init(Gattaca self_genome){
+        genome = self_genome;
+        if (G.params.save_clonal_lineage) {
+            this.genome.IncPop();
+        }
+        return this;
+    }
+
+    Cell0D Mutate(){
+        if (G.rn.Double() <  G.params.mu) {
+            // initiate new clone, with random color:
+            this.genome.DecPop();
+            this.genome = new Gattaca(this.genome, "", G.rn.Double(), G.rn.Double(), G.rn.Double(), G.rn);
+            this.genome.IncPop();
+        }
+        return this;
+    }
+
+    Cell0D Divide(){
+        return G.NewAgent().Init(genome).Mutate();
+    }
+
+    // constant death rate
+    void Death() {
+        if (G.params.save_clonal_lineage) { this.genome.DecPop(); }
+        Dispose();
+    }
+
+    void Step(){
+        if(G.rn.Double()<(G.params.DEATH_RATE + G.params.BIRTH_RATE )){
+            // death OR birth event occurs w/in this cell:
+            double birth_event_probability = G.params.BIRTH_RATE/(G.params.DEATH_RATE + G.params.BIRTH_RATE);
+
+            if(G.rn.Double()<(birth_event_probability )) {
+                Divide();
+            } else {
+                Death();
+                return;
+            }
+        }
+    }
+}
+
---
+++ GattacaExample/GattacaExample.java
@@ -0,0 +1,27 @@
+package GattacaExample;
+
+public class GattacaExample  {
+
+    public static void main(String[] args) {
+
+        boolean OVERWRITE = true; // whether or not to overwrite previous simulations
+        boolean headless = false; // whether or not to visualize in real time
+        System.setProperty("java.awt.headless", headless ? "true" : "false");
+
+        int TOTAL_SIMS = 2;
+        int  save_max_gifs = 10; // only save gifs of first n sims (to save space)
+        int modifier = 50;      // when to save data
+        int totalTime = 5000;   // total # time steps of simulation
+
+        ExampleModel0d.Nonspatial(totalTime, modifier, headless, TOTAL_SIMS, OVERWRITE);
+
+        ExampleModel2d.Square(totalTime, modifier, headless, TOTAL_SIMS, save_max_gifs, OVERWRITE);
+
+        ExampleModel3d.Cube(totalTime, modifier, headless, TOTAL_SIMS, save_max_gifs, OVERWRITE);
+
+
+        return;
+    }
+
+}
+
