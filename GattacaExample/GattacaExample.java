ebb5d2095999835a17ec02ad4f37feed17ca6aef
Jeffrey West <runningwest@gmail.com>
2020-11-18 10:41:41 -0500

    added back in "GattacaExample" -- excluding Gattaca.java

--- GattacaExample/GattacaExample.java
+++ GattacaExample/GattacaExample.java
@@ -11,18 +11,18 @@ public class GattacaExample  {
         boolean headless = false; // whether or not to visualize in real time
         System.setProperty("java.awt.headless", headless ? "true" : "false");
 
-        int TOTAL_SIMS = 10;
-        int  save_max_gifs = 10; // only save gifs of first n sims (to save space)
+        int TOTAL_SIMS = 4;
+        int  save_max_gifs = TOTAL_SIMS; // only save gifs of first n sims (to save space)
         int modifier = 50;      // when to save data
-        int totalTime = 1000;   // total # time steps of simulation
+        int totalTime = 3000;   // total # time steps of simulation
 
 
         // does not appear anywhere in simulation: (0 popsize)
 
-        ExampleModel0d.Nonspatial(totalTime, modifier, headless, TOTAL_SIMS, OVERWRITE);
+//        ExampleModel0d.Nonspatial(totalTime, modifier, headless, TOTAL_SIMS, OVERWRITE);
 
-//        ExampleModel2d.Square(totalTime, modifier, headless, TOTAL_SIMS, save_max_gifs, OVERWRITE);
-//
+        ExampleModel2d.Square(totalTime, modifier, headless, TOTAL_SIMS, save_max_gifs, OVERWRITE);
+
 //        ExampleModel3d.Cube(totalTime, modifier, headless, TOTAL_SIMS, save_max_gifs, OVERWRITE);
 
 
